65a365d256ccf28892dfab4fb29f6f1641754f7d
VarvaraShumanovich <148364310+VarvaraShumanovich@users.noreply.github.com>
2023-10-20 10:00:35 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        int a = scanner.nextInt();
+        System.out.print("Введите второе число: ");
+        int b = scanner.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        scanner.close();
+    }
+}
